8dd351fd86c00c0a963a99164017caffe213ab6f
Leo Schirmer <161379163+leo-udesc@users.noreply.github.com>
2025-03-31 17:46:21 -0300

    Add files via upload

---
+++ isaacsim/exts/isaacsim.asset.gen.conveyor.ui/docs/CHANGELOG.md
@@ -0,0 +1,53 @@
+# Changelog
+
+## [2.1.4] - 2025-01-26
+### Changed
+- Update test settings
+
+## [2.1.3] - 2025-01-21
+### Changed
+- Update extension description and add extension specific test settings
+
+## [2.1.2] - 2025-01-02
+### Changed
+- performing any UI logic only once window is built
+
+## [2.1.1] - 2024-12-11
+### Changed
+- Fix extension renaming
+## [2.1.0] - 2024-11-01
+### Changed
+- Menu name
+
+## [2.0.3] - 2024-10-28
+### Changed
+- Remove test imports from runtime
+
+## [2.0.2] - 2024-10-24
+### Changed
+- Updated dependencies and imports after renaming
+
+## [2.0.1] - 2024-10-11
+### Changed
+- Removed isaacsim core dependencies
+
+## [2.0.0] - 2024-09-27
+### Changed
+- Renamed extension to isaacsim.asset.gen.conveyor.ui
+
+## [1.1.0] - 2024-06-13
+### Fixed
+- Fixed an edge case when the first conveyor piece is added - to select the second connection point if it's a multi-connection (so it always goes forward)
+
+### Changed
+- Moved the assets source to a configurable setting
+- Moved the config path to a configurable setting
+- Moved the Conveyor Track Builder to UI.
+
+## [1.0.1] - 2024-05-06
+### Changed
+- Updated asset path
+
+## [1.0.0] - 2024-03-20
+
+- First Version
---
+++ isaacsim/exts/isaacsim.asset.gen.conveyor.ui/isaacsim/asset/gen/conveyor/ui/tests/test_builder_ui.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto. Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+import asyncio
+
+import carb
+import numpy as np
+import omni.kit.test
+
+# from omni.kit.test_suite.helpers import StageEventHandler
+import omni.kit.ui_test as ui_test
+import omni.timeline
+import omni.ui as ui
+
+
+class TestConveyorBuilderUI(omni.kit.test.AsyncTestCase):
+    async def setup(self):
+        # wait for material to be preloaded so create menu is complete & menus don't rebuild during tests
+        await omni.kit.material.library.get_mdl_list_async()
+        await ui_test.human_delay()
+
+        # TODO: get omni.kit.test_suite.
+        # self._stage_event_handler = StageEventHandler("omni.kit.stage_templates")
+
+    async def tearDown(self):
+        pass
+
+    async def testLoading(self):
+        await omni.usd.get_context().new_stage_async()
+        menu_widget = ui_test.get_menubar()
+        try:
+            await menu_widget.find_menu("Tools").click()
+            await menu_widget.find_menu("Conveyor Track Builder").click()
+        except:
+            carb.log_warn("Could not run test because carb::windowing is not available")
