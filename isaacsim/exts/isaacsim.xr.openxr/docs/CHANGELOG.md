305c265f034d27ea998d60354f38bc2032c1b992
Leo Schirmer <161379163+leo-udesc@users.noreply.github.com>
2025-08-25 11:47:36 -0300

    Add files via upload

---
+++ isaacsim/exts/isaacsim.xr.openxr/docs/CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+
+## [0.2.5] - 2025-01-21
+### Changed
+- Update extension description and add extension specific test settings
+
+
+## [0.2.4] - 2024-12-11
+### Fixed
+- Take the xr anchor offset into account in the hand joint transforms
+
+## [0.2.3] - 2024-11-19
+### Fixed
+- Transforms respect USD stage axis/units
+
+## [0.2.2] - 2024-11-13
+### Fixed
+- Specify all openxr dependancies
+
+## [0.2.1] - 2024-10-24
+### Fixed
+- Specify kitxr::openxr ext dep and fix crash if dep is missing
+
+## [0.2.0] - 2024-10-17
+### Added
+- v.0.2.0 Hand tracking python API
+
+## [0.1.0]
+### Added
+- Initial release
---
+++ isaacsim/exts/isaacsim.xr.openxr/include/isaacsim/xr/openxr/OpenXR.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+//
+// NVIDIA CORPORATION and its licensors retain all intellectual property
+// and proprietary rights in and to this software, related documentation
+// and any modifications thereto. Any use, reproduction, disclosure or
+// distribution of this software and related documentation without an express
+// license agreement from NVIDIA CORPORATION is strictly prohibited.
+//
+
+/*
+Carbonite SDK API:
+  https://docs.omniverse.nvidia.com/kit/docs/carbonite/latest/api/carbonite_api.html
+*/
+
+#pragma once
+
+#define CARB_EXPORTS
+
+#include <carb/Defines.h>
+#include <carb/Interface.h>
+#include <carb/PluginUtils.h>
+#include <carb/settings/ISettings.h>
+
+#include <omni/core/IWeakObject.h>
+#include <omni/ext/IExt.h>
+#include <omni/kit/xr/system/openxr/IOpenXRComponent.h>
+#include <omni/kit/xr/system/openxr/IOpenXRExtension.h>
+
+#include <cstdint>
+#include <optional>
+#include <vector>
+
+namespace isaacsim::xr::openxr
+{
+
+struct IOpenxr
+{
+    CARB_PLUGIN_INTERFACE("isaacsim::xr::openxr::IOpenxr", 1, 0);
+
+    virtual std::optional<std::array<XrHandJointLocationEXT, XR_HAND_JOINT_COUNT_EXT>> locate_hand_joints(
+        XrHandEXT hand, std::optional<XrTime> time, bool stageAxis) noexcept(false) = 0;
+};
+
+} // namespace isaacsim
